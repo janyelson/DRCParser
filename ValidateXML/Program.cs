996fbc60fa3d56ad780539f6cbc1e95a159fba4f
janyelson <janyelsonvictor@gmail.com>
2018-10-14 22:35:30 -0300

    first commit

---
+++ ValidateXML/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace ValidateXML
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("É necessário o argumento para o arquivo xml.");
+                return;
+            }
+
+            try { 
+                XmlReaderSettings drcSettings = new XmlReaderSettings();
+                drcSettings.Schemas.Add("http://schemas.oceanehr.com/templates", "arquivos/FICHA_DRC.xsd");
+                drcSettings.Schemas.Add("http://schemas.openehr.org/v1", "arquivos/Structure.xsd");
+                drcSettings.Schemas.Add("http://schemas.openehr.org/v1", "arquivos/BaseTypes.xsd");
+                drcSettings.ValidationType = ValidationType.Schema;
+                drcSettings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+                drcSettings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+                drcSettings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+                drcSettings.ValidationEventHandler += new ValidationEventHandler(drcSettingsValidationEventHandler);
+
+                XmlReader drc = XmlReader.Create(args[0], drcSettings);
+
+                while (drc.Read()) { }
+
+                XMLParser xml = new XMLParser(args[0]);
+                xml.Run();
+
+            } catch(FileNotFoundException e) {
+                Console.WriteLine("Argumento para o arquivo xml não encontrado. " + e.Message);
+            }
+        }
+
+        static void drcSettingsValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+                Console.WriteLine("Aviso:" + e.Message);
+            else
+                Console.WriteLine("Erro [" + e.Exception.LineNumber + "]: " + e.Message);
+        }
+    }
+}
---
+++ ValidateXML/XMLParser.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace ValidateXML
+{
+    class XMLParser
+    {
+        string xmlPath;
+
+        string path = "//tp:Ficha/tp:Dados_da_ficha/";
+        string data = "tp:data/tp:Any_event_as_Point_Event/tp:data/";
+        string value = "tp:value/";
+
+        string genero;
+        string raca;
+        string idade;
+
+        string alteracaoNaImagem;
+        string exame;
+
+        string rac;
+
+        string tfg;
+        string categoriaTfg;
+
+        string creatinina;
+
+        string albumina;
+        string categoriaAlbumina;
+
+        string equ;
+        string caracteristicasEqu;
+
+        string diabete;
+        string litiase;
+        string infeccao;
+        string policistica;
+
+        public XMLParser(string xmlPath)
+        {
+            this.xmlPath = xmlPath;
+        }
+
+        public void Run()
+        {
+            getDatas();
+            parseToOWL();
+        }
+
+        void getDatas()
+        {
+            XmlReader reader = XmlReader.Create(xmlPath);
+            XDocument doc = XDocument.Load(reader);
+            XmlNameTable nameTable = reader.NameTable;
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(nameTable);
+            namespaceManager.AddNamespace("tp", "http://schemas.oceanehr.com/templates");
+            namespaceManager.AddNamespace("v1", "http://schemas.openehr.org/v1");
+
+            //Dados do paciente
+            this.genero = doc.XPathSelectElement(path + "tp:Paciente/tp:Dados_do_paciente/" + data + "tp:Gênero/" + value + "tp:defining_code/tp:code_string", namespaceManager).Value;
+            this.raca = doc.XPathSelectElement(path + "tp:Paciente/tp:Dados_do_paciente/" + data + "tp:Raça/" + value + "tp:defining_code/tp:code_string", namespaceManager).Value;
+            this.idade = doc.XPathSelectElement(path + "tp:Paciente/tp:Dados_do_paciente/" + data + "tp:Idade/" + value + "tp:magnitude", namespaceManager).Value;
+
+            //Alteraçao de imagem
+            this.alteracaoNaImagem = doc.XPathSelectElement(path + "tp:Alteracao_de_imagem/tp:Alteracao_da_imagem/" + data + "tp:Alteração_na_imagem/" + value + "v1:value", namespaceManager).Value;
+            this.exame = doc.XPathSelectElement(path + "tp:Alteracao_de_imagem/tp:Alteracao_da_imagem/" + data + "tp:Exame/" + value + "v1:value", namespaceManager).Value;
+
+            //Exames
+            //RAC
+            this.rac = doc.XPathSelectElement(path + "tp:Exames/tp:RAC/" + data + "tp:Relação_albuminuria_creatinuria/" + value + "tp:magnitude", namespaceManager).Value;
+
+            //TFG
+            this.tfg = doc.XPathSelectElement(path + "tp:Exames/tp:TFG/" + data + "tp:TFG/" + value + "tp:magnitude", namespaceManager).Value;
+            this.categoriaTfg = doc.XPathSelectElement(path + "tp:Exames/tp:TFG/" + data + "tp:Categoria_TFG/" + value + "tp:defining_code/tp:code_string", namespaceManager).Value;
+
+            //Creatinina
+            this.creatinina = doc.XPathSelectElement(path + "tp:Exames/tp:Cretinina_Serica/" + data + "tp:Creatinina/" + value + "tp:magnitude", namespaceManager).Value;
+
+            //Nivel de albuminuria
+            this.albumina = doc.XPathSelectElement(path + "tp:Exames/tp:Nível_de_albuminuria/" + data + "tp:Nível_de_albumina/" + value + "tp:magnitude", namespaceManager).Value;
+            this.categoriaAlbumina = doc.XPathSelectElement(path + "tp:Exames/tp:Nível_de_albuminuria/" + data + "tp:Categoria/" + value + "tp:defining_code/tp:code_string", namespaceManager).Value;
+
+            //EQU EAS
+            this.equ = doc.XPathSelectElement(path + "tp:Exames/tp:EQU_EAS_Urinatipo1/" + data + "tp:Elementos_Anormais_do_Sedimento/" + value + "v1:value", namespaceManager).Value;
+            this.caracteristicasEqu = doc.XPathSelectElement(path + "tp:Exames/tp:EQU_EAS_Urinatipo1/" + data + "tp:Características/" + value + "v1:value", namespaceManager).Value;
+
+            //Fatores de Risco
+            this.diabete = doc.XPathSelectElement(path + "tp:Fatores_de_risco/tp:Outros_problemas_do_paciente/" + data + "tp:Diabetes_Mellitus/" + value + "v1:value", namespaceManager).Value;
+            this.policistica = doc.XPathSelectElement(path + "tp:Fatores_de_risco/tp:Outros_problemas_do_paciente/" + data + "tp:Doença_policistica_renal/" + value + "v1:value", namespaceManager).Value;
+            this.litiase = doc.XPathSelectElement(path + "tp:Fatores_de_risco/tp:Outros_problemas_do_paciente/" + data + "tp:Litiase_Renal/" + value + "v1:value", namespaceManager).Value;
+            this.infeccao = doc.XPathSelectElement(path + "tp:Fatores_de_risco/tp:Outros_problemas_do_paciente/" + data + "tp:Infecção_urinária_recorrente/" + value + "v1:value", namespaceManager).Value;
+
+            Print();
+        }
+
+        void parseToOWL()
+        {
+            XmlReader reader = XmlReader.Create("arquivos/Ontodrc1.1.owl");
+            XDocument doc = XDocument.Load(reader);
+            XmlNameTable nameTable = reader.NameTable;
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(nameTable);
+            namespaceManager.AddNamespace("owl", "http://www.w3.org/2002/07/owl#");
+
+            XNamespace owl = "http://www.w3.org/2002/07/owl#";
+
+            XElement element;
+
+            element = doc.XPathSelectElement("//owl:Ontology", namespaceManager);
+
+            string generoURI = "http://purl.obolibrary.org/obo/ontoneo.owl#";
+            string racaURI = "http://purl.obolibrary.org/obo/ontoneo.owl#";
+            if (genero == "at0007") generoURI += "Masculino";
+            else generoURI += "Feminino";
+
+            if (raca == "at0009") racaURI += "Preta";
+            else if(raca == "at0012") racaURI += "Parda";
+            else if (raca == "at0010") racaURI += "Branca";
+            else if (raca == "at0011") racaURI += "Amarela";
+
+            //Dados do paciente
+            //Genero
+            element.Add(new XElement(owl + "Declaration", 
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#genero"))));
+            element.Add(new XElement(owl + "ClassAssertion",
+                new XElement(owl + "Class", new XAttribute("URI", "#Genero")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#genero"))));
+            element.Add(new XElement(owl + "ObjectPropertyAssertion",
+                new XElement(owl + "ObjectProperty", new XAttribute("URI", "http://purl.obolibrary.org/obo/ontoneo.owl#temGenero")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#genero")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", generoURI))));
+            
+            //Raca
+            element.Add(new XElement(owl + "Declaration",
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#raca"))));
+            element.Add(new XElement(owl + "ClassAssertion",
+                new XElement(owl + "Class", new XAttribute("URI", "#Raca")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#raca"))));
+            element.Add(new XElement(owl + "ObjectPropertyAssertion",
+                new XElement(owl + "ObjectProperty", new XAttribute("URI", "http://purl.obolibrary.org/obo/ontoneo.owl#temRaca")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#raca")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", racaURI))));
+
+            //Idade
+            element.Add(new XElement(owl + "Declaration",
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#idade"))));
+            element.Add(new XElement("ClassAssertion",
+                new XElement(owl + "Class", new XAttribute("URI", "#Idade")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#idade"))));
+            element.Add(new XElement(owl + "DataPropertyAssertion",
+                new XElement(owl + "DataProperty", new XAttribute("abbreviatedIRI", "owl:topDataProperty")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#idade")),
+                new XElement(owl + "Literal", new XAttribute("datatypeIRI", "http://www.w3.org/2001/XMLSchema#int"), idade)));
+
+            //Alteracao de imagem
+            if(alteracaoNaImagem == "true")
+            { 
+                element.Add(new XElement(owl + "Declaration",
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#alteracao_de_imagem"))));
+                element.Add(new XElement(owl + "ClassAssertion",
+                    new XElement(owl + "Class", new XAttribute("URI", "#AlteracaoDaImagem")),
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#alteracao_de_imagem"))));
+            }
+
+            //Exame
+            //RAC
+            if (rac != null)
+            {
+                element.Add(new XElement(owl + "Declaration",
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#rac"))));
+                element.Add(new XElement(owl + "ClassAssertion",
+                    new XElement(owl + "Class", new XAttribute("URI", "#RelacaoAlbuminaCreatinina")),
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#rac"))));
+            }
+
+            //Creatinina Serica
+            element.Add(new XElement(owl + "Declaration",
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#creatinina"))));
+            element.Add(new XElement(owl + "ClassAssertion",
+                new XElement(owl + "Class", new XAttribute("URI", "#CreatininaSerica")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#creatinina"))));
+            element.Add(new XElement(owl + "DataPropertyAssertion",
+                new XElement(owl + "DataProperty", new XAttribute("abbreviatedIRI", "owl:topDataProperty")),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#creatinina")),
+                new XElement(owl + "Literal", new XAttribute("datatypeIRI", "http://www.w3.org/2001/XMLSchema#int"), creatinina)));
+
+            string classA = "#";
+            if (categoriaAlbumina == "at0006") classA += "A1";
+            else if (categoriaAlbumina == "at0007") classA += "A2";
+            else if (categoriaAlbumina == "at0008") classA += "A3";
+
+            element.Add(new XElement(owl + "Declaration",
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#albumina"))));
+            element.Add(new XElement(owl + "ClassAssertion",
+                new XElement(owl + "Class", new XAttribute("URI", classA)),
+                new XElement(owl + "NamedIndividual", new XAttribute("URI", "#albumina"))));
+
+            if (equ == "true")
+            {
+                element.Add(new XElement(owl + "Declaration",
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#equ_eas"))));
+                element.Add(new XElement(owl + "ClassAssertion",
+                    new XElement(owl + "Class", new XAttribute("URI", "#EQU_EAS_UrinaTipo1")),
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#equ_eas"))));
+            }
+
+            if(diabete == "true")
+            {
+                element.Add(new XElement(owl + "Declaration",
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#diabete"))));
+                element.Add(new XElement(owl + "ClassAssertion",
+                    new XElement(owl + "Class", new XAttribute("URI", "#DiabetesMellitus")),
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#diabete"))));
+            }
+
+            if (litiase == "true")
+            {
+                element.Add(new XElement(owl + "Declaration",
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#litiase"))));
+                element.Add(new XElement(owl + "ClassAssertion",
+                    new XElement(owl + "Class", new XAttribute("URI", "#LitiaseRenal")),
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#litiase"))));
+            }
+
+            if (policistica == "true")
+            {
+                element.Add(new XElement(owl + "Declaration",
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#policistica"))));
+                element.Add(new XElement(owl + "ClassAssertion",
+                    new XElement(owl + "Class", new XAttribute("URI", "#DoencaPolicisticaRenal")),
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#policistica"))));
+            }
+
+            if (infeccao == "true")
+            {
+                element.Add(new XElement(owl + "Declaration",
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#infeccao"))));
+                element.Add(new XElement("ClassAssertion",
+                    new XElement(owl + "Class", new XAttribute("URI", "#InfeccaoUrinariaRecorrente")),
+                    new XElement(owl + "NamedIndividual", new XAttribute("URI", "#infeccao"))));
+            }
+
+            doc.Save("arquivos/Resultado.owl");
+
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Dados do paciente: ");
+            Console.WriteLine("Gênero: " + genero);
+            Console.WriteLine("Raça: " + raca);
+            Console.WriteLine("Idade: " + idade);
+
+            Console.WriteLine();
+            Console.WriteLine("Alteracao da imagem: ");
+            Console.WriteLine("Teve alteração: " + alteracaoNaImagem);
+            Console.WriteLine("Exame: " + exame);
+
+            Console.WriteLine();
+            Console.WriteLine("TFG: ");
+            Console.WriteLine("Valor: " + tfg);
+            Console.WriteLine("Categoria: " + categoriaTfg);
+
+            Console.WriteLine();
+            Console.WriteLine("RAC: ");
+            Console.WriteLine("Valor: " + rac);
+
+            Console.WriteLine();
+            Console.WriteLine("Creatinina serica: ");
+            Console.WriteLine("Valor: " + creatinina);
+
+            Console.WriteLine();
+            Console.WriteLine("Nivel de albuminuria: ");
+            Console.WriteLine("Valor: " + albumina);
+            Console.WriteLine("Categoria: " + categoriaAlbumina);
+
+            Console.WriteLine();
+            Console.WriteLine("EQU EAS: ");
+            Console.WriteLine("Valor: " + equ);
+            Console.WriteLine("Caracteristicas: " + caracteristicasEqu);
+
+            Console.WriteLine();
+            Console.WriteLine("Fatores de risco: ");
+            Console.WriteLine("Diabetes: " + diabete);
+            Console.WriteLine("Policistica: " + policistica);
+            Console.WriteLine("Litiase: " + litiase);
+            Console.WriteLine("Infecção: " + infeccao);
+
+        }
+    }
+}
